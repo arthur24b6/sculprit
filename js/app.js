4b9bc9865f0f043b5b1bd9e107c2d014291b3f4e
Arthur <arthur@24b6.net>
2014-01-04 14:49:01 -0500

    Combines the app and content js files

--- js/app.js
+++ js/app.js
@@ -15,9 +15,9 @@ settings = YAML.eval(settings.responseText);
 var templates = {};
 // Note: for Safari and Chrome the load can't be async as the template is not
 // available when twig.render() is called.
-templates.postsTemplate = twig({
-  id: "posts",
-  href: "/templates/posts.twig",
+templates.home = twig({
+  id: "home",
+  href: "/templates/home.twig",
   async: false
 });
 
@@ -35,10 +35,10 @@ templates.footer = twig({
 
 
 var headerHTML = twig({ ref: "header" }).render(settings);
-$('#header').html(headerHTML);
+$('body header').html(headerHTML);
 
 var footerHTML = twig({ ref: "footer" }).render(settings);
-$('#footer').html(footerHTML);
+$('body footer').html(footerHTML);
 
 
 
@@ -95,6 +95,122 @@ function renderPost(post) {
 /* *********************************************** */
 /* URL routing.                                    */
 /* *********************************************** */
+
+
+/**
+ * Get all the posts in the specified directory.
+ *
+ * @returns array of post URIs
+ */
+function getAllPosts(directory, type) {
+  var posts = {};
+  // Get the directory listing. Note that the sorting is being done by Apache's
+  // list options. If this isn't supported on the server this won't work.
+  $.ajax({url: settings.content_directory, async: false, data: 'C=M;O=D'})
+    .done(function(data) {
+      var items = $(data).find('a');
+      $(items).each(function() {
+        // Apache writes the URLs relative to the directory.
+        var uri = settings.content_directory + '/' + $(this).attr('href');
+        var extension = uri.substr((~-uri.lastIndexOf(".") >>> 0) + 2);
+
+        // Does the file extension match the content type?
+        if (settings.content_type == extension) {
+
+          // Get the modified date. Apache list the entry like this:
+          // <a href="2013-11-30-twig.md">2013-11-30-twig.md</a>      02-Dec-2013 10:20  182
+          var date = $(this)[0].nextSibling.nodeValue;
+          date = $.trim(date);
+          // Now trim the file size off the end of it.
+          date = $.trim(date.replace(/[\s\S][0-9]*$/, ''));
+
+          // Check to see if this post already exists. If so, use the URI as the
+          // key.
+          if (typeof posts[date] === 'undefined') {
+            key = date;
+          }
+          else {
+            key = uri;
+          }
+          posts[key] = uri;
+        }
+      });
+
+    });
+  return posts;
+}
+
+
+/**
+ * Get a post and retun an object with body text markdown rendered.
+ *
+ * @TODO needs to handle tags listing correctly in the metadata of
+ * posts
+ *
+ * @returns post
+ */
+function getAPost(uri) {
+  var post = {};
+
+  $.ajax({url: uri, async: false})
+    .done(function(data) {
+
+      // Get the configuration settings for this post.
+      var configuration = data.match(/---([.\S\s]*?)---/);
+      var options = YAML.eval(configuration[1]);
+      $.each(options, function(key, value) {
+        post[key] = value;
+      });
+
+      // Strip out the YAML markup from the text.
+      var regex = /---[.\S\s]*?---/igm;
+      data = data.replace(regex, '');
+
+      // Convert text to Markdown.
+      var converter = new Markdown.Converter();
+      post.text = converter.makeHtml(data);
+
+      // Create a read more link.
+      post.more = '<a href="' + createAURI(uri) + '">Read more</a>';
+    });
+
+  return post;
+}
+
+
+/**
+ * Utility function to map a content file to a URI.
+ *
+ * @NOTE This function needs to be expanded in the future to handle
+ * more complex URI structure to content, handle file extensions,
+ * etc.
+ */
+function createAURI(path) {
+  // Get the filename.
+  var filename = path.replace(/^.*[\\\/]/, '');
+  return settings.posts_path + '/' + filename.substr(0, filename.lastIndexOf('.'));
+}
+
+
+/**
+ * Utility function to map a URL request to the content file path.
+ *
+ * @NOTE This function needs to be expanded in the future to handle
+ * more complex URI structures to content, handle file extensions,
+ * etc.
+ *
+ * @param {type} req
+ * @returns {undefined}
+ */
+function getAPath(request) {
+  // Right now we know that /post/ID will map to /posts/ID.md
+  return settings.content_directory + '/' + request.params['post'] + '.md';
+}
+
+
+/* *********************************************** */
+/* URL routing.                                    */
+/* *********************************************** */
 app = Davis(function () {
 
   // Ensure that routing is loaded on page load.
@@ -121,13 +237,12 @@ app = Davis(function () {
 
       // Support for updated post times.
       if (typeof post.updated !== 'undefined') {
-        //var date = strtotime(post.updated);
-        //post.date_sort = date;
+        var date = strtotime(post.updated);
+        post.date_sort = date;
       }
 
       posts.push(post);
     });
-
 
     // Sort the posts by the date_sort.
     posts.sort(function(a, b) { return  b.date_sort - a.date_sort });
@@ -135,15 +250,15 @@ app = Davis(function () {
     // Get the first post.
     var featured_post = posts.slice(0,1)[0];
 
-    // Render the full first post.
-    $('#content').html(renderPost(featured_post));
-
     // Get the remainder of the posts.
     posts = posts.slice(1);
 
     // Render the additional posts.
-    var postsHTML = twig({ ref: "posts" }).render({'posts' : posts});
-    $('#sidebar').append(postsHTML);
+    var html = twig({ ref: "home" }).render({
+      'posts' : posts,
+      'featured': featured_post
+    });
+    $('#content').html(html);
   });
 
 
